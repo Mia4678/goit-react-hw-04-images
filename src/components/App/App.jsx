20d2eb9267fd09b11760269b61aebe19b6f78a9f
Mia4678 <solomia.lutska@gmail.com>
2023-10-11 13:03:49 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,44 +16,6 @@ export function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [largeImageURL, setLargeImageURL] = useState('');
 
-  // const handleSubmit = query => {
-  //   setSearchQuery(query);
-  //   setImages([]);
-  //   setPage(1);
-  // };
-
-  // useEffect(() => {
-  //   if (page === 1) {
-  //     setImages(null);
-  //   }
-  //   fetchImages();
-  //   fetchPhotos();
-  // }, [page, searchQuery]);
-
-  // const fetchPhotos = async () => {
-  //   try {
-  //     setIsLoading(true);
-
-  //     const data = await fetchImages(searchQuery, page);
-
-  //     if (page === 1) {
-  //       setImages(data.hits);
-  //     } else {
-  //       setImages(prevImages => [...prevImages, ...data.hits]);
-  //     }
-  //   } catch (error) {
-  //     setError(error.response.data);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (page === 1) {
-  //     setImages(null);
-  //   }
-  // }, [page, searchQuery, fetchPhotos]);
-
   useEffect(() => {
     const fetchPhotos = async () => {
       if (!searchQuery) return;
@@ -86,6 +48,7 @@ export function App() {
   };
 
   const handleSearchQuery = value => {
+    // if (!searchQuery.trim());
     setSearchQuery(value);
     setPage(1);
   };
